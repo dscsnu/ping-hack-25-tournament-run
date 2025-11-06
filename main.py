0224c54d4a7e9246e25b909a61c9b8c363c51a10
Sabarigirish Manikandan <sabari.girish2057@gmail.com>
2025-11-06 19:26:23 +0530

    match function started

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from tabulate import tabulate
 from os import listdir, getcwd
 from binarytree import Node
+from importlib import import_module
+from ping_game_theory_25 import HistoryEntry, Move
+
+CLASS_NAME: str = "Bot"
 
 scores: dict[
     str, dict[str, int]
@@ -8,10 +12,69 @@ scores: dict[
 
 
 def match(p1: str, p2: str) -> str:
+    p1_cls = getattr(import_module(p1), CLASS_NAME)
+    p2_cls = getattr(import_module(p2), CLASS_NAME)
+
+    p1_obj = p1_cls()
+    p2_obj = p2_cls()
+
+    try:
+        strategy = p1_cls()
+    except Exception as e:
+        raise AssertionError(f"Strategy failed to initialize: {e}")
+    try:
+        opponent = p2_cls()
+    except Exception as e:
+        raise AssertionError(f"Strategy failed to initialize: {e}")
+
+    history_self: list[HistoryEntry] = []
+    history_opp: list[HistoryEntry] = []
+    print("Testing against RandomStrategy")
+    start_time = time.time()
+    try:
+        move_self = strategy.begin()
+        move_opp = opponent.begin()
+    except Exception as e:
+        raise AssertionError(f"begin() raised an exception: {e}")
+    if not isinstance(move_self, Move):
+        raise AssertionError(f"begin() returned invalid type: {type(move_self)}")
+    history_self.append(HistoryEntry(self=move_self, other=move_opp))
+    history_opp.append(HistoryEntry(self=move_opp, other=move_self))
+    for _ in tqdm(range(self.ROUNDS - 1), desc="Running rounds"):
+        if time.time() - start_time > StrategyTester.TIMEOUT_SECONDS:
+            raise TimeoutError(
+                f"Execution exceeded timeout of {StrategyTester.TIMEOUT_SECONDS} seconds"
+            )
+        try:
+            move_self = strategy.turn(tuple(history_self))
+        except Exception as exc:
+            raise AssertionError(f"turn() raised an exception: {exc}")
+        move_opp = opponent.turn(tuple(history_opp))
+        if not isinstance(move_self, Move):
+            raise AssertionError(f"turn() returned invalid type: {type(move_self)}")
+        history_self.append(HistoryEntry(self=move_self, other=move_opp))
+        history_opp.append(HistoryEntry(self=move_opp, other=move_self))
+        if (
+            move_self == Move.ROCK
+            and move_opp == Move.SCISSOR
+            or move_self == Move.PAPER
+            and move_opp == Move.ROCK
+            or move_self == Move.SCISSOR
+            and move_opp == Move.ROCK
+        ):
+            self.wins += 1
+        elif move_self != move_opp:
+            self.losses += 1
+        else:
+            self.draws += 1
+    total_time = time.time() - start_time
+    print(f"✅ PASS: {StrategyTester.ROUNDS} rounds in {total_time:.2f} seconds")
+    print(f"{self.wins} Wins, {self.losses} Losses, {self.draws} Draws")
+
     return p1
 
 
-def update_score(res: str, p1: str, p2: str):
+def update_score(res: str, p1: str, p2: str) -> None:
     if res == p1:
         scores[p1]["wins"] += 1
         scores[p1]["points"] += 3
@@ -25,7 +88,7 @@ def update_score(res: str, p1: str, p2: str):
         scores[p2]["points"] += 1
 
 
-def run_tournament(strategies: list[str]):
+def run_tournament(strategies: list[str]) -> None:
     for p1 in strategies:
         for p2 in strategies:
             if p1 == p2:
@@ -34,14 +97,14 @@ def run_tournament(strategies: list[str]):
             update_score(match(p1, p2), p1, p2)
 
     scoreboard = []
-    k = 0
+    i = 0
     for item in scores.items():
         scoreboard.append([])
-        scoreboard[k].append(item[0])
-        scoreboard[k].append(item[1]["wins"])
-        scoreboard[k].append(item[1]["losses"])
-        scoreboard[k].append(item[1]["points"])
-        k += 1
+        scoreboard[i].append(item[0])
+        scoreboard[i].append(item[1]["wins"])
+        scoreboard[i].append(item[1]["losses"])
+        scoreboard[i].append(item[1]["points"])
+        i += 1
 
     print(
         tabulate(
@@ -55,10 +118,9 @@ def run_tournament(strategies: list[str]):
     #     f.write(to_save)
     #     f.write("\n")
     #     f.write("-" * 30)
-    #
 
 
-def run_playoffs():
+def run_playoffs() -> None:
     (first, second, third, fourth) = list(scores.keys())[:4]
 
     first_r2: str = match(first, second)
@@ -84,7 +146,7 @@ if __name__ == "__main__":
         filter(lambda fname: fname[-2:] == "py", listdir(getcwd()))
     )
     for submission in submission_files:
-        net_id = submission.strip().lower().split("_")[1].split(".")[0]
+        net_id = submission.strip().lower()
         strategies.append(net_id)
         scores[net_id] = {"wins": 0, "losses": 0, "points": 0}
 
